8a9b6aa4cfdb4ef71808d29a21357af4e7888054
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-02-07 08:04:57 -0700

    setting up scripts

---
+++ open_project.ps1
@@ -0,0 +1,3 @@
+
+& "${env:ProgramFiles}\JetBrains\IntelliJ IDEA Community Edition 2020.3.1\bin\idea64.exe" C:\Code\ExtremeFakeItTillYouMakeIt.Java\pom.xml
+& "${env:ProgramFiles}\Google\Chrome\Application\chrome.exe" https://github.com/isidore/ExtremeFakeItTillYouMakeIt-Kata/blob/master/Fake%20it%20Till%20you%20make%20it.md
--- src/test/java/org/learnwithllew/FizzBuzzTest.java
+++ src/test/java/org/learnwithllew/FizzBuzzTest.java
@@ -1,20 +1,22 @@
 package org.learnwithllew;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.learnwithllew.utils.Tracker;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest
 {
   @Test
   public void test()
   {
-    Tracker.ResetAndLaunch();// Comment out after 1st run
+    //Tracker.ResetAndLaunch();// Comment out after 1st run
     Tracker.TrackTestRuns(this::Check20);
   }
   private void Check20()
   {
-    assertEquals(2, 2);
+    String expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    assertEquals(expected, FizzBuzz.print(20));
+
   }
 }
