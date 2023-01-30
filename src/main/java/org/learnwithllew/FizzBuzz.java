f7703f26b5dcd4047d5b471198f9635a4fafb5f1
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-30 14:20:17 -0500

    R seperate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -1,12 +1,12 @@
 package org.learnwithllew;
 
 public class FizzBuzz {
-    public static String print(int i)
-    {
+    public static String print(int i) {
         int one = 1;
         String coma = ",";
         String firstPart = "";
-        firstPart += one + coma + 2 + coma;
+        firstPart += one + coma;
+        firstPart += 2 + coma;
         String expected2 = firstPart + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected2;
     }
