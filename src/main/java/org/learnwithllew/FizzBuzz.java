3d107b595ae56a090b8ee32a6b0c57dbcf56678d
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-30 14:17:11 -0500

    r enpatuslate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -4,7 +4,8 @@ public class FizzBuzz {
     public static String print(int i)
     {
         int one = 1;
-        String expected2 = one + ","+"2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String coma = ",";
+        String expected2 = one + coma +"2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected2;
     }
 }
