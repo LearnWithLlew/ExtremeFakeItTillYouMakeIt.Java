a2a3ff176b1e77799a21764da081ec3bfcf0b352
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-30 14:35:02 -0500

    R Automate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -4,19 +4,21 @@ public class FizzBuzz {
     public static String print(int i) {
         String coma = ",";
         String firstPart = "";
-        for (int j = 1; j <= 14; j++) {
-            if (j %3 == 0) {
+        for (int j = 1; j <= 15; j++) {
+            if (j % 15 == 0) {
+                firstPart += "FizzBuzz,";
+
+            } else if (j % 3 == 0) {
                 firstPart += "Fizz,";
 
-            } else if (j % 5 == 0 ) {
+            } else if (j % 5 == 0) {
                 firstPart += "Buzz,";
 
             } else {
                 firstPart += j + coma;
             }
         }
-        firstPart += "FizzBuzz,";
-        String expected2 = firstPart +  "16,17,Fizz,19,Buzz";
+        String expected2 = firstPart + "16,17,Fizz,19,Buzz";
         return expected2;
     }
 }
