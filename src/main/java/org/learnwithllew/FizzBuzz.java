f5afc10d23ee51241588be7e812d3b26efd73545
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-30 14:22:40 -0500

    r caclulate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -6,7 +6,7 @@ public class FizzBuzz {
         String coma = ",";
         String firstPart = "";
         for (int j = 1; j <= 1; j++) {
-            firstPart += one + coma;
+            firstPart += j + coma;
         }
         firstPart += (one + 1) + coma;
         String expected2 = firstPart + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
