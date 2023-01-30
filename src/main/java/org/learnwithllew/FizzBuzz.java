4f54bbcb883fbd7abc7216c94e2f7f2dbc1e70bd
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-30 14:23:11 -0500

    R Automate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -5,10 +5,9 @@ public class FizzBuzz {
         int one = 1;
         String coma = ",";
         String firstPart = "";
-        for (int j = 1; j <= 1; j++) {
+        for (int j = 1; j <= 2; j++) {
             firstPart += j + coma;
         }
-        firstPart += (one + 1) + coma;
         String expected2 = firstPart + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected2;
     }
