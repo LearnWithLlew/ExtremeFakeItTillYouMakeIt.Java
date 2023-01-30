081dd86f76ac9cccf4df40e040d124b8418190b8
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-30 14:24:08 -0500

    R seperate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
         for (int j = 1; j <= 2; j++) {
             firstPart += j + coma;
         }
-        String expected2 = firstPart + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String expected2 = firstPart + "Fizz,"+ "4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected2;
     }
 }
