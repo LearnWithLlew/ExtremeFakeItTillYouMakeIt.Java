ba1fd65f73ca5ba5362e0dbd44f28c55c566eebf
Llewellyn Falco <llewellyn.falco@gmail.com>
2023-01-30 14:15:46 -0500

    R seperate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -3,7 +3,7 @@ package org.learnwithllew;
 public class FizzBuzz {
     public static String print(int i)
     {
-        String expected2 = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String expected2 = "1"+ ",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected2;
     }
 }
