f21856dfdc3f07c0079d7f8d866db103197f2a79
isidore <llewellyn.falco@gmail.com>
2023-02-27 18:17:19 +0000

    t add TCR
    Co-Authored-By: Lars Eckart <4414802+larseckart@users.noreply.github.com>

--- src/test/java/org/learnwithllew/FizzBuzzTest.java
+++ src/test/java/org/learnwithllew/FizzBuzzTest.java
@@ -1,16 +1,18 @@
 package org.learnwithllew;
 
+import com.github.larseckart.tcr.TestCommitRevertExtension;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.learnwithllew.utils.Tracker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@ExtendWith(TestCommitRevertExtension.class)
 public class FizzBuzzTest
 {
   @Test
   public void test()
   {
-    //Tracker.ResetAndLaunch();// Comment out after 1st run
+    Tracker.ResetAndLaunch();// Comment out after 1st run
     Tracker.TrackTestRuns(this::Check20);
   }
   private void Check20()
