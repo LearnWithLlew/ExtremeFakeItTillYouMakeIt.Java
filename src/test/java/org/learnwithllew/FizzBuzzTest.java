12abebd5c416574f4a54fd58caa7f55c9d891b24
isidore <llewellyn.falco@gmail.com>
2023-02-27 19:43:23 +0000

    R separate

--- src/test/java/org/learnwithllew/FizzBuzzTest.java
+++ src/test/java/org/learnwithllew/FizzBuzzTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.learnwithllew.utils.Tracker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//@ExtendWith(TestCommitRevertExtension.class)
+@ExtendWith(TestCommitRevertExtension.class)
 public class FizzBuzzTest
 {
   @Test
