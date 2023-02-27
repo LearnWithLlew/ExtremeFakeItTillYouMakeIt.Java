74870bb040b7d17e1b4bb288b48b9b9d8dee5ca3
isidore <llewellyn.falco@gmail.com>
2023-02-27 19:34:16 +0000

    r encapsulate
    Co-Authored-By: kotadeepthi <16993346+kotadeepthi@users.noreply.github.com>

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -3,7 +3,8 @@ package org.learnwithllew;
 public class FizzBuzz {
     public static String print(int i)
     {
-        String expected = "1" + ",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String number = "1";
+        String expected = number + ",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected;
     }
 }
