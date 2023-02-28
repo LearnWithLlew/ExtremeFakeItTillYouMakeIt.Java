29e5a65b080e042b13d71ed7ef0069aa3f65bf90
isidore <llewellyn.falco@gmail.com>
2023-02-28 19:32:54 +0000

    r clean

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -5,8 +5,8 @@ public class FizzBuzz {
     {
         String number = "" + 1;
 
-        String choice1 = ",";
-        String expected = number + choice1 + "2" + ",Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String separator = ",";
+        String expected = number + separator + "2" + ",Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected;
     }
 }
