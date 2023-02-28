bd64c0a100152f659242bb642210c7ed0e15c839
isidore <llewellyn.falco@gmail.com>
2023-02-28 19:47:24 +0000

    r encapsulate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -6,7 +6,8 @@ public class FizzBuzz {
         String number = "" + 1;
         String separator = ",";
         String anotherNumber = "2";
-        String expected = number + separator + anotherNumber + separator + "Fizz" + separator + "4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String result = number + separator + anotherNumber + separator + "Fizz" + separator;
+        String expected = result + "4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected;
     }
 }
