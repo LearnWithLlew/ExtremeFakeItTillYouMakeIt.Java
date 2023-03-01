ac59b5c6faed1fde17eca9b4535d3f4072b9f26b
isidore <llewellyn.falco@gmail.com>
2023-03-01 19:35:26 +0000

    R automate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -8,10 +8,9 @@ public class FizzBuzz {
         String anotherNumber = "2";
         String fizz = "Fizz";
         String result = "";
-        for (int x = 1; x < 2; x++) {
+        for (int x = 1; x < 3; x++) {
             result += x + separator;
         }
-        result += anotherNumber + separator;
         result += fizz + separator;
         String expected = result + "4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected;
