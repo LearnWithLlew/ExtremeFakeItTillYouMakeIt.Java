f97b5d40b4715207bc0f26894aa9025ea3cbb5aa
isidore <llewellyn.falco@gmail.com>
2023-03-02 19:14:43 +0000

    R Separate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -17,7 +17,7 @@ public class FizzBuzz {
             }
         }
 
-        String expected = result + "Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String expected = result + "Buzz," + "Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected;
     }
 }
