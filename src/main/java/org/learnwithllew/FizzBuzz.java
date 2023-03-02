b1720782ff1edf6c679e4d54f38da7db49f0f2ef
isidore <llewellyn.falco@gmail.com>
2023-03-02 19:35:16 +0000

    R automate

--- src/main/java/org/learnwithllew/FizzBuzz.java
+++ src/main/java/org/learnwithllew/FizzBuzz.java
@@ -9,11 +9,11 @@ public class FizzBuzz {
         String fizz = "Fizz";
         String buzz = "Buzz";
         String result = "";
-        for (int x = 1; x <= 9; x++) {
+        for (int x = 1; x <= 10; x++) {
             if (x == 3 || x == 6 || x == 9) {
                 result += fizz + separator;
             }
-            else if(x == 5) {
+            else if(x == 5 || x == 10) {
                 result += buzz + separator;
             }
             else {
@@ -21,7 +21,7 @@ public class FizzBuzz {
             }
         }
 
-        String expected = result + "Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+        String expected = result + "11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
         return expected;
     }
 }
