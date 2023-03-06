4ad7515931d5be60af3a9553b3df5b66ca2e5ad0
isidore <llewellyn.falco@gmail.com>
2023-03-06 19:48:38 +0000

    t added test

--- src/test/java/org/learnwithllew/FizzBuzzTest.java
+++ src/test/java/org/learnwithllew/FizzBuzzTest.java
@@ -7,19 +7,21 @@ import org.learnwithllew.utils.Tracker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(TestCommitRevertExtension.class)
-public class FizzBuzzTest
-{
+public class FizzBuzzTest {
 
   @Test
-  public void test()
-  {
+  public void test() {
     //Tracker.ResetAndLaunch();// Comment out after 1st run
     Tracker.TrackTestRuns(this::Check20);
   }
-  private void Check20()
-  {
+
+  private void Check20() {
     String expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
     assertEquals(expected, FizzBuzz.print(20));
+  }
 
+  private void Check15() {
+    String expected = "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz";
+    assertEquals(expected, FizzBuzz.print(20));
   }
 }
